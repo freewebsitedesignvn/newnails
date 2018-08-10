86225435d9bd12926b811b37888476987341bd73
Mai Trung Dzung <maidung1983@gmail.com>
2018-08-10 10:54:54 +0700

    Change name controller Contact

--- application/controllers/home/Contact.php
+++ application/controllers/home/Contact.php
@@ -3,7 +3,7 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Contact extends CI_Controller {
 
-    public $page='trangchu';
+    public $page='Contact';
     
     function __construct() {
         parent::__construct();        
